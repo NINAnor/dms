cf23750cf216150bbf089cc856ecb703e72a793e
Niccolò Cantù <niccolo.cantu@nina.no>
2025-02-19 11:41:09 +0100

    dev: fix helpers

--- helpers.sh
+++ helpers.sh
@@ -5,9 +5,7 @@ BASE_CMD="docker compose --profile"
 export HOSTNAME
 
 alias dpcli_dev="$BASE_CMD dev"
-alias dpcli_maps="$BASE_CMD maps"
 alias dpcli_prod="$BASE_CMD prod"
 
-alias djcli_dev="dpcli_dev exec -it django-dev python manage.py"
-alias djcli_maps="dpcli_maps exec -it django-dev python manage.py"
-alias djcli_prod="dpcli_prod exec -it django python manage.py"
+alias djcli_dev="dpcli_dev exec -it django-dev uv run manage.py"
+alias djcli_prod="dpcli_prod exec -it django uv run manage.py"
