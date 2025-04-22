d97810402b90323c0e61fe7f5c14e88451aa6040
Niccolo Cantu <niccolo.cantu@nina.no>
2025-04-22 09:47:22 +0200

    fix: close old connections

--- src/dms/core/tasks.py
+++ src/dms/core/tasks.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.core.management import call_command
+from django.db import close_old_connections
 from procrastinate.contrib.django import app
 
 
 @app.periodic(cron="* 0 * * *")
 @app.task(name="sync_ldap")
 def sync_ldap(timestamp: int):
+    close_old_connections()
     if settings.AUTH_LDAP_SERVER_URI:
         call_command("sync_ldap_users")
