6b5356f33a12e692112e5a875c8d7dbab290c0bb
Niccolo Cantu <niccolo.cantu@nina.no>
2025-04-24 15:12:16 +0200

    chore: remove and update dependencies

--- src/dms/core/management/commands/setup.py
+++ src/dms/core/management/commands/setup.py
@@ -1,10 +1,6 @@
-import traceback
-
-from countries_plus.models import Country
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
-from languages_plus.models import Language
 
 User = get_user_model()
 
@@ -13,14 +9,3 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(is_superuser=True).exists():
             call_command("loaddata", "users.json")
-
-        if not Country.objects.all().exists():
-            try:
-                call_command("update_countries_plus")
-            except Exception:
-                print(traceback.format_exc())
-                print("Falling back to local fixture")
-            call_command("loaddata", "countries.json.gz")
-
-        if not Language.objects.all().exists():
-            call_command("loaddata", "languages_data.json.gz")
