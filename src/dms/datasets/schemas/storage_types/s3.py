c0e3ec368ce775987209c81b49425dc617b0b3c4
Niccolo Cantu <niccolo.cantu@nina.no>
2025-04-24 15:52:30 +0200

    chg: remove s3 config

--- src/dms/datasets/schemas/storage_types/s3.py
+++ src/dms/datasets/schemas/storage_types/s3.py
@@ -6,20 +6,6 @@ schema = {
             "title": "Endpoint",
             "help_text": "The endpoint for the S3 storage",
         },
-        "bucket": {
-            "type": "string",
-            "title": "Bucket",
-            "help_text": "The bucket for the S3 storage",
-        },
-        "access_key": {
-            "type": "string",
-            "title": "Access Key",
-            "help_text": "The access key for the S3 storage",
-        },
-        "secret_key": {
-            "type": "string",
-            "title": "Secret Key",
-        },
     },
-    "required": ["endpoint", "bucket"],
+    "required": ["endpoint"],
 }
