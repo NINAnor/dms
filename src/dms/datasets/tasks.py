3d411d878b5a758804c628b834fc84bb6838e6b0
Niccolo Cantu <niccolo.cantu@nina.no>
2025-05-12 16:49:26 +0200

    fix: close old connections

--- src/dms/datasets/tasks.py
+++ src/dms/datasets/tasks.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.db import close_old_connections
 from procrastinate.contrib.django import app
 
 from .conf import settings
@@ -10,6 +11,7 @@ from .models import Dataset
 @app.periodic(cron=settings.DATASETS_IPT_CRON)
 @app.task(name="datasets:harvest__ipt")
 def harvest_ipt_task(timestamp: int):
+    close_old_connections()
     harvester = ipt.IPTHarvester()
     for url in settings.DATASETS_IPT_URLS:
         logging.info(f"fetching {url}")
@@ -18,5 +20,6 @@ def harvest_ipt_task(timestamp: int):
 
 @app.task(name="datasets:harvest__ipt__dataset")
 def harvest_ipt_dataset_task(dataset_id):
+    close_old_connections()
     dataset = Dataset.objects.get(id=dataset_id)
     ipt.IPTResourceHarvester().run(dataset=dataset)
