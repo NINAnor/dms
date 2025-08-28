d48edea5aefef3bc2973a0e442e0b8c6fb2a6607
Niccolo Cantu <niccolo.cantu@nina.no>
2025-08-28 15:51:50 +0200

    chg: enable token athentication for the API

--- src/dms/core/settings/base.py
+++ src/dms/core/settings/base.py
@@ -408,6 +408,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
