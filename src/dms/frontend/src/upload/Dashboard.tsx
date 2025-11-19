e55be0b1e1c62500435673ff350f034ea41cbfe5
Niccolo Cantu <niccolo.cantu@nina.no>
2025-11-19 09:28:45 +0100

    fix: colors, descriptions and backlink

--- src/dms/frontend/src/upload/Dashboard.tsx
+++ src/dms/frontend/src/upload/Dashboard.tsx
@@ -8,7 +8,15 @@ export function Dashboard() {
 
   return (
     <div>
-      <h1 className="font-bold text-3xl mb-5">Upload resources</h1>
+      <div className="flex justify-between mb-3">
+        <h1 className="font-bold text-3xl">Upload resources</h1>
+        <a href="../../" className="btn btn-primary">
+          Back
+        </a>
+      </div>
+      <p className="mb-5">
+        Uploaded files will be processed in background, it could take some time before they are shown in the dataset
+      </p>
       <UppyDashboard id="dashboard" uppy={uppy!} />
     </div>
   );
