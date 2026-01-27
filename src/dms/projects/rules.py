61f50f272e8dfc221e2ff220019f93ebb297c012
Niccolo Cantu <niccolo.cantu@nina.no>
2026-01-27 14:20:37 +0100

    chg: simplify rules with composition

--- src/dms/projects/rules.py
+++ src/dms/projects/rules.py
@@ -25,32 +25,15 @@ def is_project_participant(user, project):
 
 @rules.predicate
 def is_owner(user, instance):
-    if not user.is_authenticated:
-        return False
     if not instance:
         return True
     return instance.owner == user
 
 
-@rules.predicate
-def is_dmp_project_participant(user, dmp):
-    if not user.is_authenticated:
-        return False
-    if not dmp:
-        return True
-    if dmp and not dmp.project:
-        return dmp.owner == user
-    return dmp.project.memberships.filter(id=user.id).exists()
-
-
 def dmp_project_role_is(role):
     @rules.predicate
     def has_role_in_project(user, dmp):
-        if not user.is_authenticated:
-            return False
-        if not dmp.project and dmp.owner == user:
-            return True
-        if dmp.project:
+        if user.is_authenticated and dmp.project:
             return dmp.project.members.filter(user=user, role=role).exists()
         return False
 
