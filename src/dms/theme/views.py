a3dd6325e1ed33d67294ab335f1c43cd559c9b5e
Niccolo Cantu <niccolo.cantu@nina.no>
2026-03-04 22:18:25 +0100

    feat: featured services

--- src/dms/theme/views.py
+++ src/dms/theme/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from dms.services.models import Resource
+
 
 class HomeView(TemplateView):
     template_name = "theme/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["featured_resources"] = Resource.objects.filter(featured=True).order_by(
+            "featured_order"
+        )
+        return context
